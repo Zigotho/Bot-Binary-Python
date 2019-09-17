afbf390f37be1bc3695204424b0f6794e7f9c201
Matheus de Carvalho <matheusc.j@hotmail.com>
2019-09-17 19:43:18 -0300

    Corrigido o bug do Login, e do Payout

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 
 #  Abrir pagina
 driver = webdriver.Chrome()
-driver.get('https://webtrader.binary.com/')
+driver.get('https://webtrader.binary.com/')  # site da binary
 driver.maximize_window()
 action = ActionChains(driver)
 sleep(3)
@@ -13,12 +13,13 @@ user = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/button').clic
 sleep(10)
 
 #  Fazer Login
-btn_login = driver.find_element_by_xpath("/html/body/nav/div[3]/div/div[2]/ul[1]/li[2]/button").click()
+btn_login = driver.find_element_by_xpath(
+    "/html/body/nav/div[3]/div/div[2]/ul[1]/li[2]/button").click()  # Primeiro botao de login
 sleep(4)
-btn_login2 = driver.find_element_by_xpath('/html/body/div[8]/div[2]/div/div[1]/button').click()
+btn_login2 = driver.find_element_by_xpath('//*[@id="windows-dialog-3"]/div/div[1]/button').click()  # segundo Botao
 sleep(5)
-txt_email = driver.find_element_by_id('txtEmail').send_keys('altere isso')
-txt_senha = driver.find_element_by_id('txtPass').send_keys('altere isso')
+txt_email = driver.find_element_by_id('txtEmail').send_keys('Mudar antes de Rodar')
+txt_senha = driver.find_element_by_id('txtPass').send_keys('Mudar antes de Rodar')
 entrar = driver.find_element_by_name('login').click()
 sleep(10)
 
@@ -38,18 +39,19 @@ action.move_to_element(janela_abrir).perform()
 janela_abrir = driver.find_element_by_xpath('/html/body/nav/div[3]/div/ul/li[1]/ul/li[4]/ul/li[1]/ul/li[4]')  # 10
 janela_abrir.click()
 sleep(2)
-metodo_entrada = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/ul[9]/li[1]/span/span[1]')
+metodo_entrada = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/ul[9]/li[1]/span/span[2]')
 metodo_entrada.click()
-metodo_entrada = driver.find_element_by_id('ui-id-575').click()
+metodo_entrada = driver.find_element_by_xpath('/html/body/div[6]/div[7]/ul/li[2]/div').click()
 payout = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/ul[9]/li[3]/input')
 payout.clear()
-payout.send_keys('0.35')
+payout.send_keys(input('Qual o valor da sua entrada: '))
 # olhar o tick e dar entradas
 azul = 0
 tick = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/ul[1]/li[4]/span')
 tick_atual = 0
 vermelho = 0
 comprado = False
+VelasVermelhas = int(input('Quantos velas vermelhas para entrar de compra? '))
 while True:
 
     if tick.text != tick_atual:
@@ -63,7 +65,7 @@ while True:
             vermelho = 0
         tick_atual = tick.text
         print(f'Tick na variavel {tick_atual}, tick no site {tick.text}')
-    if vermelho >= 4 and not comprado:
+    if vermelho >= VelasVermelhas and not comprado:
         comprar = driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/ul[16]/li[2]/button').click()
         comprado = True
         sleep(15)
